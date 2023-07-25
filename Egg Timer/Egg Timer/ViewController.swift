743434213cffc567b6a7eb8723fbb4a0ed6c27aa
Privetyanikita <72889842+Privetyanikita@users.noreply.github.com>
2023-07-25 20:08:32 +0200

    аdded pictures, added functionality

--- Egg Timer/Egg Timer/ViewController.swift
+++ Egg Timer/Egg Timer/ViewController.swift
@@ -8,12 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
+    
+    var totalTime = 0
+    var secondPassed = 0
+    
+    var timer = Timer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        guard let hardness = sender.titleLabel?.text else { return }
+        
+        timer.invalidate()
+        
+        totalTime = eggTimes[hardness]!
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        progressView.progress = 0.0
+        secondPassed = 0
+        
 
+    
+    }
+    
+    @objc func updateTimer() {
+        if secondPassed < totalTime {
+            
+            secondPassed += 1
+            let percentageProgress = Float(secondPassed) / Float(totalTime)
+            titleLabel.text = "\(totalTime - secondPassed) seconds"
+            progressView.progress = percentageProgress
+        
+        } else {
+            timer.invalidate()
+            titleLabel.text = "Done!"
+        
+        }
+    }
+    
 }
 
